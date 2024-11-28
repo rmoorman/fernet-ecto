ef6f891b547cbc99140f6cef4b8cc4103138f300
Rico Moorman <rico.moorman@gmail.com>
2024-11-28 01:07:07 +0100

    Cleanup

--- mix.exs
+++ mix.exs
@@ -23,22 +23,10 @@ defmodule Fernet.Ecto.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [extra_applications: [:logger, :crypto]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:earmark, "~> 1.4", only: [:dev]},
